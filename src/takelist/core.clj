f5f6a60f81a06e0a9442c27c9966a89432f97226
Mathias Rühle <math2306@hotmail.com>
2017-08-29 19:40:14 +0200

    Configure uberjar profile
    Define main method for standalone execution.

--- src/takelist/core.clj
+++ src/takelist/core.clj
@@ -1,4 +1,24 @@
-(ns takelist.core)
+(ns takelist.core
+  (:require [aleph.http :as http]
+            [environ.core :refer [env]]
+            [takelist.app :refer [app]])
+  (:gen-class))
+
+
+(def db {:classname "org.h2.Driver"
+         :subprotocol "h2:file"
+         :subname (env :database-uri)})
+
+(def base-uri (env :base-uri))
+
+(def client-id (env :client-id))
+
+(def client-secret (env :client-secret))
 
 (defn -main [& args]
-  )
+  (let [server (http/start-server (app (assoc env :db db
+                                                :base-uri base-uri
+                                                :client-id client-id
+                                                :client-secret client-secret))
+                                {:port 8080})]
+    (.wait-for-close server)))
